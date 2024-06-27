da9d69fbcc5807cded35c490c7307bd7f9258014
david14dx <147641797+david14dx@users.noreply.github.com>
2024-06-28 05:01:22 +0800

    J285AM

--- src/views/EntranceMode.vue
+++ src/views/EntranceMode.vue
@@ -13,87 +13,146 @@
 
         <!-- Main Content Grid -->
         <div class="row">
-            <!-- Left Column: Employee Image and Details -->
+            <!-- Left Column: Image and Details -->
             <div class="col-md-4 mb-4">
                 <div class="card">
-                    <img class="card-img-top" :src="image || 'https://via.placeholder.com/150'" alt="Employee Image">
+                    <img class="card-img-top" :src="image || 'https://via.placeholder.com/150'" alt="Image">
                     <div class="card-body">
                         <p class="card-text text-center">{{ lastName }}, {{ firstName }}</p>
                     </div>
                 </div>
             </div>
 
-            <!-- Right Column: Employee Data and Timekeeping -->
+            <!-- Right Column: Data and Timekeeping -->
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
-                        <h4>Employee Details</h4>
+                        <h4>{{ userType === 'EMPLOYEE' ? 'Employee Details' : 'Student Details' }}</h4>
                     </div>
                     <div class="card-body">
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Employee ID:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="empID" disabled>
+                        <div v-if="userType === 'EMPLOYEE'">
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Employee ID:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="empID" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Last Name:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="lastName" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">First Name:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="firstName" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Middle Name:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="middleName" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Position:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="position" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Department:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="department" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Birthday:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="bday" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Is Active:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="isActive" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Employee Type:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="empType" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Note:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="note" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Schedule ID:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="schedID" disabled>
+                                </div>
                             </div>
                         </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Last Name:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="lastName" disabled>
+
+                        <div v-else-if="userType === 'STUDENT'">
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Student ID:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="studID" disabled>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">First Name:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="firstName" disabled>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Last Name:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="lastName" disabled>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Middle Name:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="middleName" disabled>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">First Name:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="firstName" disabled>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Position:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="position" disabled>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Middle Name:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="middleName" disabled>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Department:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="department" disabled>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Course:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="course" disabled>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Birthday:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="bday" disabled>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Year Level:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="yearLevel" disabled>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Is Active:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="isActive" disabled>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Birthday:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="bday" disabled>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Employee Type:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="empType" disabled>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Is Active:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="isActive" disabled>
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Note:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="note" disabled>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label class="col-sm-4 col-form-label">Schedule ID:</label>
-                            <div class="col-sm-8">
-                                <input type="text" class="form-control" v-model="schedID" disabled>
+                            <div class="form-group row">
+                                <label class="col-sm-4 col-form-label">Note:</label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control" v-model="note" disabled>
+                                </div>
                             </div>
                         </div>
 
@@ -122,38 +181,56 @@ import axios from 'axios';
 
 const searchID = ref('');
 const empID = ref('');
+const studID = ref('');
 const lastName = ref('');
 const firstName = ref('');
 const middleName = ref('');
 const position = ref('');
 const department = ref('');
+const course = ref('');
+const yearLevel = ref('');
 const bday = ref('');
 const isActive = ref('');
 const empType = ref('');
 const image = ref('');
 const note = ref('');
 const schedID = ref('');
+const userType = ref('');
 const currentDate = ref('');
 
 // Function to fetch RFID data
 function searchRFID() {
-    axios.get('https://rjprint10.com/entrancemonitoring/backend/rfidapi.php?action=get_by_id&RFID=' + searchID.value)
+    axios.get('https://rjprint10.com/entrancemonitoring/backend/timekeepingapi.php?action=get_by_id&RFID=' + searchID.value)
         .then(response => {
             const data = response.data;
             if (data) {
-                empID.value = data.empID;
-                lastName.value = data.lastName;
-                firstName.value = data.firstName;
-                middleName.value = data.middleName;
-                position.value = data.position;
-                department.value = data.department;
-                bday.value = data.bday;
-                isActive.value = data.isActive;
-                empType.value = data.empType;
-                image.value = data.image;
-                note.value = data.note;
-                schedID.value = data.schedID;
-                insertLogEmp(data.userID);
+                userType.value = data.userType;
+                if (userType.value === 'EMPLOYEE') {
+                    empID.value = data.empID;
+                    lastName.value = data.lastName;
+                    firstName.value = data.firstName;
+                    middleName.value = data.middleName;
+                    position.value = data.position;
+                    department.value = data.department;
+                    bday.value = data.bday;
+                    isActive.value = data.isActive;
+                    empType.value = data.empType;
+                    image.value = data.image;
+                    note.value = data.note;
+                    schedID.value = data.schedID;
+                    insertLog(data.userID);
+                } else if (userType.value === 'STUDENT') {
+                    studID.value = data.studID;
+                    lastName.value = data.lastName;
+                    firstName.value = data.firstName;
+                    middleName.value = data.middleName;
+                    course.value = data.course;
+                    yearLevel.value = data.yearLevel;
+                    bday.value = data.bday;
+                    isActive.value = data.isActive;
+                    note.value = data.note;
+                    insertLog(data.userID);
+                }
             } else {
                 clearFields();
             }
@@ -163,31 +240,34 @@ function searchRFID() {
         });
 }
 
-// Function to insert log for employee
-function insertLogEmp(userID) {
-    const newRecord = {
+// Function to insert log
+function insertLog(userID) {
+    const newLog = {
         action: 'create',
-        empID: userID,
+        userID: userID,
         logType: "TimeIn or PC Name or Param", // Update with appropriate log type
     };
 
-    axios.post('https://rjprint10.com/entrancemonitoring/backend/timekeepingapi.php', newRecord)
+    axios.post('https://rjprint10.com/entrancemonitoring/backend/timekeepingapi.php', newLog)
         .then(response => {
             // Handle success
         })
         .catch(error => {
-            console.error("Error saving record: ", error);
+            console.error("Error saving log: ", error);
         });
 }
 
 // Function to clear form fields
 function clearFields() {
     empID.value = '';
+    studID.value = '';
     lastName.value = '';
     firstName.value = '';
     middleName.value = '';
     position.value = '';
     department.value = '';
+    course.value = '';
+    yearLevel.value = '';
     bday.value = '';
     isActive.value = '';
     empType.value = '';
